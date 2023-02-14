240f3338e797e4ddbe210229514810b08f5d291e
Alessandro Leotta <alessandro.leotta@iesnervion.com>
2023-02-14 20:13:50 +0100

    .

--- Practice Exercises - Arrays/src/exercise1/Functions.java
+++ Practice Exercises - Arrays/src/exercise1/Functions.java
@@ -3,3 +3,15 @@ import java.util.*;
 
 public class Functions {
 }
+/*Realizaremos el juego del buscaminas con un array unidimensional.
+El juego nos proporcionará pistas, de forma que si destapamos una casilla y no hay una mina,
+esta casilla nos indicará cuantas minas hay adyacentes a esa posición.
+Por lo tanto el ordenador debe preparar una tabla de 20 casillas para nosotros
+en el que colocará 6 minas y las pistas correspondientes.
+Ejemplo: {0,1,*,1,1,*,*,2,*,1,0,0,0}
+Este panel permanecerá oculto y es el jugador el que debe tratar de descubrirlo.
+En cada turno, el jugador indicará qué casilla quiere destapar.
+Si en esa casilla no hay ninguna mina, se le debe indicar al jugador la pista que hay en esa casilla.
+Si hay una mina, se le mostrará un mensaje al jugador indicando que ha perdido.
+El juego debe seguir ejecutándose hasta que el jugador destape una mina, o haya destapado todas las pistas.
+*/
--- Practice Exercises - Arrays/src/exercise2/Functions.java
+++ Practice Exercises - Arrays/src/exercise2/Functions.java
@@ -1,4 +1,5 @@
 package exercise2;
+import java.util.*;
 
 public class Functions {
 }
